865dc21d0e6df76fc996353f04c0da00e9cca568
Anton <anton.d.mingov@gmail.com>
2026-01-06 22:59:35 +0200

    infra: define Terraform versions and input variables

---
+++ terraform/variables.tf
@@ -0,0 +1,35 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "project_name" {
+  description = "Project name for resource naming"
+  type        = string
+  default     = "gallery-core"
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "production"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.small"
+}
+
+variable "key_pair_name" {
+  description = "Name of AWS key pair for SSH access"
+  type        = string
+}
+
+variable "volume_size" {
+  description = "Root volume size in GB"
+  type        = number
+  default     = 20
+}
+
